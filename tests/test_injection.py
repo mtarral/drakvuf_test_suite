6a300ecd4eb963da1292373936058f59a065f481
Mathieu Tarral <mathieu.tarral@ssi.gouv.fr>
2019-07-17 15:42:47 +0200

    remove injection method specific test
    Now we specify the injection method in the command line

--- tests/test_injection.py
+++ tests/test_injection.py
@@ -1,13 +1,6 @@
-import logging
-
-# GUEST_TEST_BIN = 'C:\\Users\\Jonas\\Desktop\\stress_test.ps1'
-GUEST_TEST_BIN = 'C:\\Windows\System32\\reg.exe'
 PLUGIN_LIST = ['procmon', 'crashmon', 'bsodmon']
-INJECTION_CANDIDATE = 'taskmgr'
-INJECTION_METHOD = 'shellexec'
 
-
-def test_shellexec(ev_queue):
+def test_injection(ev_queue):
     for event in iter(ev_queue['queue'].get, None):
         assert(event['Plugin'] != 'crashmon')
         assert (event['Plugin'] != 'bsodmon')

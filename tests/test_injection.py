c82866a5a278f6e7cd02f7f447e1885729b7fbc4
Mathieu Tarral <mathieu.tarral@ssi.gouv.fr>
2019-11-19 14:20:13 +0100

    add comments

--- tests/test_injection.py
+++ tests/test_injection.py
@@ -1,3 +1,8 @@
+# the PLUGIN_LIST constant declares the plugins required for the test
+# here our test_injection will only need crashmon and bsodmon.
+# however, we also include procmon and filetracer, because this is required
+# for every test to follow the guest process execution and determine when the
+# execution has been completed.
 PLUGIN_LIST = ['procmon', 'filetracer', 'crashmon', 'bsodmon']
 
 
